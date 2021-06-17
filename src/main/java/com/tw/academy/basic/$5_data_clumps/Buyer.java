2f52a78a085b30e4d043f69460715be84235a3bf
LIUJO5 <joi.liu@cargosmart.com>
2021-06-17 15:46:10 +0800

    d

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Buyer.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Buyer.java
@@ -1,41 +1,30 @@
 package com.tw.academy.basic.$5_data_clumps;
 
 public class Buyer {
+    private String name;
+    private String phoneNumber;
+    private String address;
+
     public Buyer(String name, String phoneNumber, String address) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.address = address;
     }
 
-    private String name;
-    private String phoneNumber;
-    private String address;
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Buyer(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String confirm() {
+        return String.format("Please confirm buyer information: buyer name is %s, " +
+                "buyer phone number is %s and buyer address is %s.", this.getName(), this.getPhoneNumber(), this.getAddress());
     }
 }
--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -10,23 +10,12 @@ public class Order {
         this.buyer = buyer;
     }
 
-    public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
-        this.orderNumber = orderNumber;
-
-        this.setBuyer(buyerName, buyerPhoneNumber, buyerAddress);
-    }
-
     public Buyer getBuyer() {
         return buyer;
     }
 
-    public void setBuyer(String buyerName, String buyerPhoneNumber, String buyerAddress) {
-        this.buyer = new Buyer(buyerName, buyerPhoneNumber, buyerAddress);
-    }
-
     public String confirm() {
-        return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", this.getBuyer().getName(), this.getBuyer().getPhoneNumber(), this.getBuyer().getAddress());
+        return getBuyer().confirm();
     }
 }
 
