fc26812267562d03d8bf0e64012333bfdabbb5c5
LIUJO5 <joi.liu@cargosmart.com>
2021-06-17 20:59:45 +0800

    extract receiver as existed class User

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -1,25 +1,19 @@
 package com.tw.academy.basic.$5_data_clumps;
 
 public class Parcel {
+    private final User user;
     private String size;
     private Double weight;
-
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String receiverAddress;
 
     private String senderName;
     private String senderPhoneNumber;
     private String senderAddress;
 
     public Parcel(String size, Double weight,
-                  String receiverName, String receiverPhoneNumber, String receiverAddress,
-                  String senderName, String senderPhoneNumber, String senderAddress) {
+                  User user, String senderName, String senderPhoneNumber, String senderAddress) {
         this.size = size;
         this.weight = weight;
-        this.receiverName = receiverName;
-        this.receiverPhoneNumber = receiverPhoneNumber;
-        this.receiverAddress = receiverAddress;
+        this.user = user;
         this.senderName = senderName;
         this.senderPhoneNumber = senderPhoneNumber;
         this.senderAddress = senderAddress;
@@ -28,7 +22,7 @@ public class Parcel {
     public String confirmReceiver(){
         return String.format("Please confirm receiver information: receiver name is %s, " +
                         "receiver phone number is %s and receiver address is %s.\n",
-                receiverName, receiverPhoneNumber, receiverAddress);
+                this.user.getBuyerName(), this.user.getBuyerPhoneNumber(), this.user.getBuyerAddress());
     }
 
     public String confirmSender(){
